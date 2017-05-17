46acf1624cf80fd1a540faa5eb32fbd2627c435f
mtcattin <cat16002@byui.edu>
2017-05-17 15:12:22 -0600

    Added Planning section in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,30 @@
 //this is the main.js file
+
+/**************************************************
+  PLANNING A PROJECT:
+-----------------------------------------------
+1. What do you want to make?:
+-----------------------------------------------
+MC:
+>
+>
+
+SC:
+>
+
+-----------------------------------------------
+2. What steps do you think are necessary?
+-----------------------------------------------
+
+
+
+-----------------------------------------------
+3. What syntax or coding patterns might you use?
+-----------------------------------------------
+
+
+-----------------------------------------------
+Remember: There are many ways to create things with code! The most important part is to write it and get it working.  If you can't get something to work, try making it simpler.  
+After, look for ways you might improve your code! 
+
+**************************************************/
